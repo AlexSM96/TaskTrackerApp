51b8681e3f2959d9fe1608bf8823c4ef214de642
александр сычугов <salexm74@gmail.com>
2026-02-23 00:02:49 +0500

    исапрелно route

--- tasktracker-app/src/services/AxiosWithAuthorization.js
+++ tasktracker-app/src/services/AxiosWithAuthorization.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5037' || import.meta.env.REACT_APP_API_URL || 'http://localhost:5037',
+    baseURL: import.meta.env.REACT_APP_API_URL || 'http://localhost:5037',
     timeout: 100000,
     headers: {"Authorization": `Bearer ${localStorage.getItem('token')}`}
 });
